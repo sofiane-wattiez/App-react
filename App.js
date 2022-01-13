f33d086a9a6e3d94d6f55c80126ba5831e7e5bdf
swattiez <swattiez@fr.scc.com>
2022-01-13 19:54:25 +0100

    add fetch apit for GET winereds

--- App.js
+++ App.js
@@ -2,6 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
+  const callApi = async () => {
+    let result = await fetch(
+      'https://localhost:8000/api/wine_reds',
+      {
+       method: 'GET' 
+      //  headers:'Access-Control-Allow-Origin'
+      })
+      .then((response) => {return response.json()})
+      console.log(result)
+  }
+  useEffect(() => {
+    callApi()
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
