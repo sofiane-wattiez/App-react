9174845947219de0d3a376221ad6beccb0990dc3
swattiez <swattiez@fr.scc.com>
2022-01-13 20:11:35 +0100

    Env fonctionnel avec exemple requete get

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// import React, { useState, useEffect } from 'react';
 
 export default function App() {
-  
-  const callApi = async () => {
-    let result = await fetch(
-      'https://localhost:8000/api/wine_reds',
-      {
-       method: 'GET' 
-      //  headers:'Access-Control-Allow-Origin'
-      })
-      .then((response) => {return response.json()})
-      console.log(result)
-  }
-  useEffect(() => {
-    callApi()
-  }, [])
+  // Exemple d'une requete get avec chemin fonctionnel en revanche il faut lancer un serveur symfony en parrallele pour pouvoirs 
+  // Exécuter des requetes actuellement dans le temps ont vérra si ont se prend un serveur web pour que l'api sois tous le temps dispo 
+  // Je vous brief Demain
+  // 
+  // const callApi = async () => {
+  //   let result = await fetch(
+  //     'https://localhost:8000/api/wine_reds',
+  //     {
+  //      method: 'GET' 
+  //     //  headers:'Access-Control-Allow-Origin'
+  //     })
+  //     .then((response) => {return response.json()})
+  //     console.log(result)
+  // }
+  // useEffect(() => {
+  //   callApi()
+  // }, [])
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Wine Search </Text>
       <StatusBar style="auto" />
     </View>
   );
